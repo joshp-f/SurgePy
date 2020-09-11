767ceba9da913be10a1a19bd9d4471f85e6dd87c
joshpf <panflop@ozemail.com.au>
2020-09-11 14:06:24 +1000

    Fix - windows paths supported

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,17 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+// import [slash from 'slash';
+const slash = require('slash');
+```
+Test Cases - 
+linux and window paths
+nested folders
+correct values
+```
+// vsce publish
 class Surge {
-	surgeTypesData : {[id:string]:string};
+	surgeTypesData : {[id:string]:{file:string,name:string,lineno:number,args:string}};
 	constructor() {
 		this.surgeTypesData = {};
 
@@ -15,22 +23,29 @@ class Surge {
 			const string = val.toString();
 			this.surgeTypesData = JSON.parse(string);
 		});
-	}
-	getFunctionArgumentsHovertext(functionName: string) {
-		const validate = (name: string) =>{
-			return (name in this.surgeTypesData);
-		};
-		const transform = (name:string) => {
-			const data = this.surgeTypesData[name];
-			return JSON.stringify(data);
-		};
-		if(!validate(functionName)) {
-			return '';
-		}
-		const output = transform(functionName);
-		return output;
+  }
+  getLineData({file,line}:{file:string,line:number}) {
+    const results = Object.values(this.surgeTypesData).filter(d => d.file === file && d.lineno === line);
+    if (!results.length) {
+      return null;
+    }
+    return results[0];
+  }
+	// getFunctionArgumentsHovertext(functionName: string) {
+	// 	const validate = (name: string) =>{
+	// 		return (name in this.surgeTypesData);
+	// 	};
+	// 	const transform = (name:string) => {
+	// 		const data = this.surgeTypesData[name];
+	// 		return JSON.stringify(data.args) + ' => ' + data.returnval;
+	// 	};
+	// 	if(!validate(functionName)) {
+	// 		return '';
+	// 	}
+	// 	const output = transform(functionName);
+	// 	return output;
 
-	}
+	// }
 }
 const surge = new Surge();
 // this method is called when your extension is activated
@@ -43,7 +58,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const fsWatcher = vscode.workspace.createFileSystemWatcher(
 		'**/.surgetypes'
-	);
+  );
 	fsWatcher.onDidChange((uri) => {
 		surge.createTypesData(uri);	
 	});
@@ -55,10 +70,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.languages.registerHoverProvider('python', {
     provideHover(document, position, token) {
-			const wordRange = document.getWordRangeAtPosition(position);
-			const word = document.getText(wordRange);
-			const typeText = surge.getFunctionArgumentsHovertext(word);
-			return new vscode.Hover(typeText);
+			// const wordRange = document.getWordRangeAtPosition(position);
+      // const word = document.getText(wordRange);
+      // const typeText = surge.getFunctionArgumentsHovertext(word);
+      const transformed_filename = slash(document.fileName)
+      .split("/")
+      .slice(-1)[0];
+      const line_data = surge.getLineData({file:transformed_filename,line:position.line+1});
+      if (!line_data) return new vscode.Hover("");
+			return new vscode.Hover(`${line_data.args}`);
     }
 	}));
 
